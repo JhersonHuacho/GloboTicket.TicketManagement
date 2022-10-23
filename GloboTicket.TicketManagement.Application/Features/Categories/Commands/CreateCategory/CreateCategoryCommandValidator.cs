a10c2c8004786b311a14825be8a1d1c3e2adce12
Francisco <jhersonhuacho@gmail.com>
2022-10-23 07:56:59 -0500

    Setting up the Application Core

---
+++ GloboTicket.TicketManagement.Application/Features/Categories/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Commands.CreateCategory
+{
+    public class CreateCategoryCommandValidator: AbstractValidator<CreateCategoryCommand>
+    {
+        public CreateCategoryCommandValidator()
+        {
+
+        }
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Features/Categories/Queries/GetCategoriesListWithEvents/GetCategoriesListWithEventsQueryHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GloboTicket.TicketManagement.Application.Features.Categories.Queries.GetCategoriesListWithEvents
+{
+    public class GetCategoriesListWithEventsQueryHandler : IRequestHandler<GetCategoriesListWithEventsQuery, List<CategoryEventListVm>>
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public GetCategoriesListWithEventsQueryHandler(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CategoryEventListVm>> Handle(GetCategoriesListWithEventsQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _categoryRepository.GetCategoriesWithEvents(request.IncludeHistory);
+            return _mapper.Map<List<CategoryEventListVm>>(list);
+        }
+    }
+}
